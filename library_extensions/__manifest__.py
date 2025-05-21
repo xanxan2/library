3d469d0b186e62788e43c12bd094d187fb86289e
Roxanne Teneza <33974697+xanxan2@users.noreply.github.com>
2025-05-21 08:18:56 +0800

    odoo assessment

---
+++ library_extensions/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Library Extension",
+    "version": "1.0",
+    "depends": [
+        "base",
+        "library"
+    ],
+    "external_dependencies": {},
+    "author": "Roxanne Teneza",
+    "summary": """Library Extensions""",
+    "description": """
+        Library Extensions
+    """,
+    "data": [
+        "security/ir.model.access.csv",
+        "views/library_category_views.xml",
+        "views/library_book_views.xml",
+    ],
+    "installable": True,
+    "application": False,
+}
---
+++ library_extensions/models/library_book.py
@@ -0,0 +1,16 @@
+from odoo import models, fields
+
+class LibraryBook(models.Model):
+    _inherit = "library.book"
+
+    author_id = fields.Many2one(
+        "res.partner", 
+        string="Author", 
+        required=True
+    )
+
+    
+    category_ids = fields.Many2many(
+        'library.book.category',
+        string='Categories'
+    )
